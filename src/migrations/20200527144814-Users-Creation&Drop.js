219109f8a82648ba88609d03d1278369ca2c9f78
Nico Goldman <nicolasgoldman07@gmail.com>
2020-05-28 09:50:36 -0300

    Add sequelize initialization
    + Add User model
    + Add User create/drop migration
    + Add Config for connecting to PostreSQL

---
+++ src/migrations/20200527144814-Users-Creation&Drop.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      salt: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
---
+++ src/models/user.js
@@ -0,0 +1,32 @@
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      // A default unique universal identifier generated
+      // Why not autoincremental?
+      defaultValue: DataTypes.UUIDV4,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+};
