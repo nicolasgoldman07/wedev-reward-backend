2d10f68085d103e8e31967421a3284c54f863786
Nico Goldman <nicolasgoldman07@gmail.com>
2020-06-02 11:40:24 -0300

    Added crypto security with password generated by salt

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,10 @@
+import crypto from 'crypto';
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define('user', {
     id: {
       type: DataTypes.UUID,
       primaryKey: true,
-      // A default unique universal identifier generated
-      // Why not autoincremental?
       defaultValue: DataTypes.UUIDV4,
     },
     username: {
@@ -29,6 +29,31 @@ export default (sequelize, DataTypes) => {
       allowNull: true,
     },
   });
+
+  // Instance methods
+  User.prototype.passwordMatches = function (value) {
+    return User.encryptPassword(value, this.salt) === this.password;
+  };
+
+  // Class methods
+  User.hashPasswordHook = async function (user) {
+    if (!user.password || !user.changed('password')) return user;
+
+    user.salt = this.getRandomSalt();
+    user.password = await User.encryptPassword(user.password, user.salt);
+  };
+
+  User.getRandomSalt = function (bytes = 16) {
+    return crypto.randomBytes(bytes).toString('hex');
+  };
+
+  User.encryptPassword = function (plainPassword, salt) {
+    return crypto.scryptSync(plainPassword, salt, 64).toString('hex');
+  };
+
+  // hooks
+  User.beforeCreate(User.hashPasswordHook.bind(User));
+  User.beforeUpdate(User.hashPasswordHook.bind(User));
 
   return User;
 };
