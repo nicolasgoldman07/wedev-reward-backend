8042a56ca89a3578034a27fac5f9a457d9c6e850
Nico Goldman <nicolasgoldman07@gmail.com>
2020-06-29 14:02:22 -0300

    Added passport for authentication with JWT
    + Added passport & passport-jwt packages
    + Added passport strategy (jwt) for getting every request auth header and setting the context
    ! Missing error and info handlers with jwt auth result

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 
+import passport from 'passport';
+
 import schema from './schema';
 import models from './models';
 import resolvers from './schema/resolvers';
@@ -15,11 +17,44 @@ const app = express();
 const server = new ApolloServer({
   ...schema,
   resolvers,
-  context: { models },
+  context: async ({ req, res }) => ({ models, user: req.user }),
   instrospection: true,
   playground: true,
   tracing: true,
 });
+
+app.use(function (req, res, next) {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    // JWT invalid
+    // if (info) return next(new error_types.Error401(info.message));
+
+    // DB Query error
+    // if (err) return next(err);
+
+    // No user match with JWT
+    // if (!user)
+    //   return next(new error_types.Error403('You are not allowed to access.'));
+
+    // User data into request
+    req.user = user;
+    console.log('request user:', req.user);
+    next();
+  })(req, res, next);
+});
+
+var JwtStrategy = require('passport-jwt').Strategy,
+  ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+passport.use(
+  new JwtStrategy(opts, async function (jwt_payload, done) {
+    return done(
+      null,
+      await models.user.findOne({ where: { id: jwt_payload.sub } }),
+    );
+  }),
+);
 
 server.applyMiddleware({ app });
 
@@ -30,6 +65,6 @@ server.installSubscriptionHandlers(httpServer);
 httpServer.listen({ port }, () => {
   console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
   console.log(
-    `Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`
+    `Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`,
   );
 });
