de25c633cd2a2c430a116df4fd7f6c15df42bae6
Nico Goldman <nicolasgoldman07@gmail.com>
2020-07-14 13:42:06 -0300

    Implement authentication with passport

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 
+import passportInit from './passportJwt';
+
 import schema from './schema';
 import models from './models';
 import resolvers from './schema/resolvers';
@@ -15,11 +17,13 @@ const app = express();
 const server = new ApolloServer({
   ...schema,
   resolvers,
-  context: { models },
+  context: async ({ req, res }) => ({ models, user: req.user }),
   instrospection: true,
   playground: true,
   tracing: true,
 });
+
+app.use(passportInit);
 
 server.applyMiddleware({ app });
 
@@ -30,6 +34,6 @@ server.installSubscriptionHandlers(httpServer);
 httpServer.listen({ port }, () => {
   console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`);
   console.log(
-    `Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`
+    `Subscriptions ready at ws://localhost:${port}${server.subscriptionsPath}`,
   );
 });
